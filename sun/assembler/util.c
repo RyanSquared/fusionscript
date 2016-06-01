e2ef2f7143b187c3fe2100ed71fcc2280158179d
Ryan Rion <vandor2012@gmail.com>
2016-06-01 05:16:21 -0500

    sun/assembler/util.c: add function sun_convert_literal_string()
    util.convert_literal_string(string) takes a single string and returns it
    as a Lua bytecode compatible string - beginning with a size_t
    demonstrating the size of the string including a terminating '\0' at the
    end, then followed by the string including the terminating '\0'

--- sun/assembler/util.c
+++ sun/assembler/util.c
@@ -1,5 +1,6 @@
 // vim:set noet sts=0 sw=2 ts=2:
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <lua.h>
@@ -32,7 +33,7 @@ int sun_grab_luac_data(lua_State *L) {
 }
 
 int sun_check_luac_num(lua_State *L) {
-	union LuaNumberToChar {
+	union lua_number_to_char {
 		char c[sizeof(lua_Number)];
 		lua_Number n;
 	} conv;
@@ -42,7 +43,7 @@ int sun_check_luac_num(lua_State *L) {
 }
 
 int sun_check_luac_int(lua_State *L) {
-	union LuaIntegerToChar {
+	union lua_integer_to_char {
 		char c[sizeof(lua_Integer)];
 		lua_Integer n;
 	} conv;
@@ -51,11 +52,33 @@ int sun_check_luac_int(lua_State *L) {
 	return 1;
 }
 
+int sun_convert_literal_string(lua_State *L) {
+	luaL_checkstring(L, 1);
+	int length = luaL_len(L, 1);
+	const char* input = luaL_checkstring(L, 1);
+	char* output = malloc(sizeof(size_t) + length + 1);
+	union size_t_to_char { // ^ Lua doesn't include embedded '\0'
+		char c[sizeof(size_t)];
+		size_t s;
+	} conv;
+	conv.s = length + 1;
+	for (int i=0; i < sizeof(size_t); i++) {
+		output[i] = conv.c[i]; // manually copy to avoid '\0' errors
+	}
+	for (int i=0; i < length; i++) {
+		output[sizeof(size_t) + i] = input[i];
+	}
+	output[sizeof(size_t) + length] = '\0';
+	lua_pushlstring(L, output, sizeof(size_t) + length + 1);
+	return 1;
+}
+
 static const struct luaL_Reg sun_assembler_util_lib[] = {
 	{"check_sizes", sun_check_sizes},
-	{"grab_luac_data", sun_grab_luac_data}
+	{"grab_luac_data", sun_grab_luac_data},
 	{"check_luac_num", sun_check_luac_num},
 	{"check_luac_int", sun_check_luac_int},
+	{"convert_literal_string", sun_convert_literal_string},
 	{NULL, NULL}
 };
 
