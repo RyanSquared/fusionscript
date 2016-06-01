7909fb6393a59d5547dd8143e0421892c1f0f683
Ryan Rion <vandor2012@gmail.com>
2016-05-31 23:56:21 -0500

    sun/assembler/util.c: add LUAC_DATA

--- sun/assembler/util.c
+++ sun/assembler/util.c
@@ -11,6 +11,26 @@
 #define SUN_VERSION "0.1.0"
 #define SUN_COPYRIGHT "Copyright (c) 2016 Ryan"
 
+#define sun_set_size(L, string, item) \
+	lua_pushstring(L, string);\
+	lua_pushinteger(L, sizeof(item));\
+	lua_settable(L, -3);
+
+int sun_check_sizes(lua_State *L) {
+	lua_newtable(L);
+	sun_set_size(L, "int", int);
+	sun_set_size(L, "size_t", size_t);
+	sun_set_size(L, "Instruction", Instruction);
+	sun_set_size(L, "lua_Integer", lua_Integer);
+	sun_set_size(L, "lua_Number", lua_Number);
+	return 1;
+}
+
+int sun_grab_luac_data(lua_State *L) {
+	lua_pushliteral(L, LUAC_DATA);
+	return 1;
+}
+
 int sun_check_luac_num(lua_State *L) {
 	union LuaNumberToChar {
 		char c[sizeof(lua_Number)];
@@ -32,6 +52,8 @@ int sun_check_luac_int(lua_State *L) {
 }
 
 static const struct luaL_Reg sun_assembler_util_lib[] = {
+	{"check_sizes", sun_check_sizes},
+	{"grab_luac_data", sun_grab_luac_data}
 	{"check_luac_num", sun_check_luac_num},
 	{"check_luac_int", sun_check_luac_int},
 	{NULL, NULL}
