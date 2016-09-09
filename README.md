f34edf4a0594192c3afc76b832826d9ba0eb4e73
Charles Heywood <charlesh2016@usd402.com>
2016-09-09 02:20:58 -0500

    Multiple changes to the parser and README
    1. Changed Class to it's own set of assignments
     * Variables are done using `Name = Expression ;`
     * Methods are done using `Name ( FunctionNameList ) => StatementList`
     * `FunctionNameList` is a set of Names with optional assignments
    2. Reassignment using a binary operator (`x += y`) is now possible
    3. For statements are added
    4. Removed unnecessary comments
    5. `@` operator used in accessing instance-specific variables
    6. Using parenthesis `()` for blocks is now -required-

--- README.md
+++ README.md
@@ -5,6 +5,112 @@ The programming language of ultimate dankliness
 you know what you're doing; also, I have no clue how to write a programming
 language so good luck figuring out what the hell I'm doing.
 
+## Examples
+
+### Hello World
+
+```
+/*
+ * The example in this code example is just for testing
+ * and will not actually run as of 9/9/2016
+ */
+
+local {stdout} = require("core.async.io");
+stdout:write("Hello World!");
+```
+
+### Factorial
+
+```
+/*
+ * The example in this code example is just for testing
+ * and will not actually run as of 9/9/2016
+ */
+
+{stdout} = require("core.async.io");
+
+factorial(n)->
+    if n == 0
+        return 1;
+    else
+        return n * factorial(n-1);
+
+stdout:write(tostring(factorial(5)));
+```
+
+### Account (from Lua Demo)
+
+```
+/*
+ * The example in this code example is just for testing
+ * and will not actually run as of 9/9/2016
+ */
+
+new Account {
+    new(balance = 0)=> {
+        @balance = balance;
+    }
+
+    deposit(amount)=> {
+        @balance += amount;
+        return true;
+    }
+
+    withdraw(amount)=> {
+        if (amount > @balance)
+            return false;
+            /* one-line statement, no brackets */
+            /* comments don't count towards lines */
+        else {
+            @balance -= amount;
+            return true;
+        }
+    }
+
+    balance()=>
+        return @balance;
+}
+
+bob = Account(500); /* 500  */
+bob:deposit(600);   /* 1100 */
+bob:withdraw(1000); /* 100  */
+assert(bob:withdraw(math.max)); /* errors */
+```
+
+### Asynchronous Networking
+
+```
+/*
+ * The example in this code example is just for testing
+ * and will not actually run as of 9/9/2016
+ */
+
+local {Async} = require("core.async");
+local {TCPSocket, TCPServer} = require("core.async.net");
+
+new ExampleAsyncApp extends Async {
+    priority = {"server", "client"};
+
+    client()-> {
+        socket = TCPSocket("localhost", 9999);
+        socket:send("echo");
+        print(socket:recv(4) == "echo");
+        socket:close();
+    }
+
+    server()-> {
+        server = TCPServer("localhost", 9999);
+        local client = server:accept();
+        local input = client:recv(1024);
+        client:send(input);
+        client:close();
+        server:close();
+    }
+}
+
+ExampleAsyncApp:run();
+```
+
 ## Compiling Utilities Library
 
 **Please note** that this method is proven to work on Linux systems only. The
--- fusion/parser/init.lua
+++ fusion/parser/init.lua
@@ -1,17 +1,13 @@
--- Import LPeg variables
-
 local lpeg = require("lpeg")
-local P, V, S, R = lpeg.P, lpeg.V, lpeg.S
+local P, V, S, R = lpeg.P, lpeg.V, lpeg.S, lpeg.R
 
--- Somewhat out of place patterns
-
-local Space      = P" "
-local WS         = S" \t\r\n\v" ^ 0
-local EndOfLine  = P"\r" ^ -1 * P"\n"
-local Shebang    = P"#!" * P(1 - EndOfLine) ^ 0 * EndOfLine
+local Space       = P" "
+local WSPre       = S" \t\r\n\v"
+local WS, WSOnce  = WSPre ^ 0, WSPre ^ 1
+local EndOfLine   = P"\r" ^ -1 * P"\n"
+local Shebang     = P"#!" * P(1 - EndOfLine) ^ 0 * EndOfLine
+local Parenthesis = #(P"(") -- lookahead
 -- ::TODO:: comments
-
--- Number parsers
 
 local Length    = (S"uU" ^ -1 * S"lL" ^ -2) ^ -1
 local Integer   = R"09" ^ 1
@@ -20,8 +16,6 @@ local DecNumber = Integer ^ -1 * (P"." * Integer)
 local IntNumber = Integer * Length
 local Extension = S"eE" * P"-" ^ -1
 local Number = (HexNumber + DecNumber + IntNumber) * Extension ^ -1
-
--- Strings
 
 local SingleQuote = P"'"
 local DoubleQuote = P'"'
@@ -33,37 +27,22 @@ local DoubleQuoteString = DoubleQuote * ((FullEscape + P(1)) - P"\\" *
     -- match either an escape sequence or single byte that is not '\"'
 
 local String = SingleQuoteString + DoubleQuoteString
-
--- Booleans
-
 local True = P"true"
 local False = P"false"
 local Boolean = True + False
-
--- Nil
-
 local Nil = P"nil"
-
--- Import LPeg variables
-
-local lpeg = require("lpeg")
-local P, R = lpeg.P, lpeg.R
-
--- Variable names
 
 local Upper  = R"AZ"
 local Lower  = R"az"
 local Letter = Upper + Lower
-
 local VariableName = (Letter + P"_") * (Letter + P"_" + Integer) ^ 0
 local Filters = P(false)
-local Keywords = {"else", "if", "true", "false", "nil", "while", "do", "new"}
-for key, word in pairs(Keywords)
+local Keywords = {"else", "if", "true", "false", "nil", "while", "in",
+    "new", "for", "extends"}
+for key, word in pairs(Keywords) do
     Filters = Filters + P(word)
 end
-local Variable = VariableName - Filters
-
--- Binary symbol pattern
+local Variable = P"@" ^ -1 * (VariableName - Filters)
 
 local UnarySymbol = S"#!~-"
 local BinarySymbol = P(false)
@@ -85,41 +64,48 @@ end
 
 local Base = P {
     "File";
-    FunctionCall        = V"PrefixExpression" * V"Arguments" + V"PrefixExpression" *
-        P":" * Variable * V"Arguments";
-    PrefixExpression    = V"Name" + V"FunctionCall" + P"(" * WS * V"Expression" *
-        WS * P")";
-    Name                = Variable + V"PrefixExpression" * WS * P"[" * WS *
-        V"Expression" * WS * P"]" + V"PrefixExpression" *WS * P"." * WS *
-        Variable;
-    Expression          = V"Literal" + P"..." + V"PrefixExpression" +
-        V"Expression" * WS * BinarySymbol * WS * V"Expression" + UnarySymbol * WS
-        * V"Expression";
-    Literal             = V"TableConstructor" + V"FunctionDeclaration" + V"Class" +
-        Number + String + Boolean + Nil;
-    Arguments           = P"(" * V"ExpressionList" * V")";
-    ExpressionList      = V"Expression" * (WS * P"," * WS * V"Expression") ^ 0;
-    TableConstructor    = P"{" * WS * V"TableFieldList" * WS * P"}";
-    TableFieldList      = V"TableField" * (WS * P"," * WS * V"TableField") ^ 0;
-    TableField          = P"[" * WS * V"Expression" * WS * P"]" * WS * P"=" *
-        V"Expression" + Variable * WS * P"=" * WS * V"Expression" +
-        V"Expression";
-    FunctionDeclaration = P"(" * WS * V"VariableList" * WS * P")" * WS * (P"-" +
-        P"=") * P">" * WS * V"StatementList";
-    VariableList        = Variable * (WS * P"," * WS * Variable) ^ 0;
-    Assignment          = V"VariableList" * WS * P"=" * WS * V"ExpressionList";
-    StatementList       = V"Statement" + P"{" * V"Statement" * (WS *
-        V"Statement") ^ 0 * P"}";
-    Statement           = (P"return" * WS * V"ExpressionList" + P";" +
-        V"Assignment" + P"break" + V"While" + V"For" + V"If" + V"Class") * WS *
-        P";";
-    While               = P"while" * WS * V"Expression" * V"StatementList";
-    For                 = (V"FunctionCall" + V"Name") * WS * (P"|>" + P"|" *
-        V"Arguments" * P">") * WS * StatementList;
-    If                  = P"if" * WS * V"Expression" * WS * StatementList *
-        (WS * P"else" * WS * V"Statement") ^ 0
-    Class               = P"new" * (WS * Variable) ^ -1 * V"TableConstructor" *
-        (WS * P"::" * WS * Variable) ^ -1  
-    File                = Shebang ^ -1 * (V"Statement" * (WS * V"Statement") ^
-        0) ^ -1
+    FunctionCall         = V"PrefixExpression" * V"Arguments" + V"PrefixExpression" * WS * P":" * WS * Variable *
+                            V"Arguments";
+    PrefixExpression     = V"Name" + V"FunctionCall" + P"(" * WS * V"Expression" * WS * P")";
+    Name                 = V"PrefixExpression" * WS * P"[" * WS * V"Expression" * WS * P"]" + V"PrefixExpression" * WS *
+                            P"." * WS * Variable + Variable;
+    Expression           = V"Literal" + P"..." + V"PrefixExpression" + V"Expression" * WS * BinarySymbol * WS *
+                            V"Expression" + UnarySymbol * WS * V"Expression";
+    Literal              = V"TableConstructor" + V"FunctionDeclaration" + V"Class" + Number + String + Boolean + Nil;
+    Arguments            = P"(" * WS * V"ExpressionList" * WS * V")";
+    ExpressionList       = V"Expression" * (WS * P"," * WS * V"Expression") ^ 0;
+    TableConstructor     = P"{" * WS * V"TableFieldList" * WS * P"}";
+    TableFieldList       = V"TableField" * (WS * P"," * WS * V"TableField") ^ 0;
+    TableField           = P"[" * WS * V"Expression" * WS * P"]" * WS * P"=" * V"Expression" + Variable * WS * P"=" *
+                            WS * V"Expression" + V"Expression";
+    VariableList         = Variable * (WS * P"," * WS * Variable) ^ 0;
+    FunctionVariable     = Variable * (WS * P"=" * WS * V"Expression");
+    FunctionVariableList = V"FunctionVariable" * (WS * P"," * WS * V"FunctionVariable") ^ 0;
+    FunctionBody         = P"(" * WS * V"FunctionVariableList" * WS * P")" * WS * S"-=" * P">" * WS * V"StatementList";
+    NamedFunction        = Variable * (WS * P"[" * WS * V"Expression" * WS * P"]") ^ 0 * WS * V"FunctionBody";
+    NamedLocalFunction   = P"local" * WSOnce * Variable * WS * V"FunctionDeclaration";
+    AnonymousFunction    = V"FunctionBody";
+    FunctionDeclaration  = V"NamedFunction" + V"NamedLocalFunction" + V"AnonymousFunction";
+    Assignment           =(P"local" * WSOnce) ^ -1 * V"VariableList" * WS * P"=" * WS * V"ExpressionList";
+    Reassignment         = Variable * WS * BinarySymbol * P"=" * WS * Expression;
+    StatementList        = V"Statement" + P"{" *  WS * V"Statement" * (WS * V"Statement") ^ 0 * P"}";
+    Statement            =(P"return" * WSOnce * V"ExpressionList" * WS + V"Assignment" + V"Reassignment" + P"break" +
+                            V"FunctionCall") * WS * P";" + V"While" + V"ForwardAssertFor" +
+                            V"IteratorFor" + V"NumericFor" + V"If" + V"Class";
+    While                = P"while" * WS * Parenthesis * V"Expression" * WS * V"StatementList";
+    ForwardAssertFor     =(V"FunctionCall" + V"Name") * WS * P"|>" * WS * V"StatementList";
+    NumericForItem       = Number + V"PrefixExpression";
+    NumericFor           = P"for" * WS * P"(" * WS * Variable * WS * P"=" * WS * V"NumericForItem" * WS * P"," * WS *
+                            V"NumericForItem" * (WS * P"," * WS * V"NumericForItem") ^ -1 * WS * P"do" * WS *
+                            V"StatementList";
+    IteratorFor          = P"for" * WS * P"(" * WS * V"VariableList" * WS * P"in" * WS * V"ExpressionList" * WS * P")" *
+                            V"StatementList";
+    If                   = P"if" * WS * P"(" * WS * V"Expression" * P")" * WS * V"StatementList" * (WS * P"else" * WS *
+                            V"StatementList") ^ -1;
+    ClassAttributeField  = Variable * WS * P"=" * WS * V"Expression" * P";";
+    ClassFunctionField   = Variable * WS * V"AnonymousFunction";
+    ClassField           = V"ClassAttributeField" + V"ClassFunctionField";
+    Class                = P"new" * (WSOnce * Variable) ^ -1 * WS * (P"extends" * WSOnce * V"Expression" * WS) ^ -1 *
+                            P"{" * (WS * V"ClassField") ^ 0 * WS * P"}";
+    File                 = Shebang ^ -1 * (V"Statement" * (WS * V"Statement") ^ 0) ^ -1;
 }
