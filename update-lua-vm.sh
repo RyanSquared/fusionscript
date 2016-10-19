454be69e2a490c87bd4143baa7a9d3243eafe1b4
Ryan Rion <charlesh2016@usd402.com>
2016-10-18 18:21:28 -0700

    update-lua-vm.sh: --color=never

--- update-lua-vm.sh
+++ update-lua-vm.sh
@@ -2,4 +2,5 @@
 
 git fetch luavm && \
 git merge -s subtree --squash luavm/master && \
-git commit -m "update luavm subtree to {$(git log luavm/master --oneline -1)}"
+git commit -m \
+"update luavm subtree to {$(git log luavm/master --oneline -1 --color=never)}"
