32621c6695957b12e893b15a26bf494a54084826
Ryan Rion <charlesh2016@usd402.com>
2016-11-07 21:41:08 -0800

    source.lua: initial parser

---
+++ fusion/core/parsers/source.lua
@@ -0,0 +1,123 @@
+local lexer = require("fusion.core.lexer")
+
+local parser = {}
+local handlers = {}
+
+function transform(node)
+	return handlers[node[1]](node)
+end
+
+function transform_expression_list(node)
+	local output = {}
+	local list = node.expression_list
+	for i=1, #list do
+		output[#output + 1] = transform(list[i])
+	end
+	return table.concat(output, ",")
+end
+
+function transform_variable_list(node)
+	local output = {}
+	local list = node.variable_list
+	for i=1, #list do
+		output[#output + 1] = transform(list[i])
+	end
+	return table.concat(output, ",")
+end
+
+handlers['assignment'] = function(node)
+	require("pl.pretty").dump(node[2])
+	local output = {}
+	if node[2].is_local then
+		output[1] = "local "
+	end
+	if node[2].variable_list.is_destructuring then
+		-- ::TODO:: change node[1] to node in lexer
+		local expression = transform(node[2].expression_list[1])
+		local last = {} -- capture all last values
+		for i, v in ipairs(node[2].variable_list) do
+			local value = transform(v)
+			last[#last + 1] = expression .. "." .. value
+			output[#output + 1] = value
+			if node[2].variable_list[i + 1] then
+				output[#output + 1] = ','
+			end
+		end
+		output[#output + 1] = " = "
+		output[#output + 1] = table.concat(last, ',')
+		return table.concat(output)
+	end
+	output[#output + 1] = transform_variable_list(node[2]) -- ::TODO::
+	output[#output + 1] = " = "
+	output[#output + 1] = transform_expression_list(node[2]) -- ::TODO::
+	return table.concat(output)
+end
+
+handlers['function_call'] = function(node)
+	local name = transform(node[2])
+	if node.expression_list then -- has expressions, return with expressions
+		return name .. "(" .. transform_expression_list(node) .. ")"
+	else
+		return name .. "()"
+	end
+end
+
+handlers['variable'] = function(node)
+	local name = {node[2]}
+	for i=3, #node do
+		if type(node[i]) == "string" then
+			if node[i]:match("^[_A-Za-z][_A-Za-z0-9]*$") then
+				name[#name + 1] = "." .. node[i]
+			else
+				name[#name + 1] = "[" .. node[i] .. "]"
+			end
+		else
+			name[#name + 1] = "[" .. transform(node[i]) .. "]"
+		end
+	end
+	return table.concat(name)
+end
+
+handlers['sqstring'] = function(node)
+	return "'" .. node[2]:gsub("\\", "\\\\") .. "'"  -- \ is ignored in '' strings
+end
+
+function parser.compile(input_stream, output_stream)
+	for input in input_stream do
+		output_stream(transform(input))
+	end
+end
+
+function parser.read_file(file, dump)
+	local append, output
+	if dump then
+		append = print
+	else
+		output = {}
+		append = function(line) output[#output + 1] = line end
+	end
+	local source_file = io.open(file)
+	local node = lexer:match(source_file:read("*a"))
+	source_file:close()
+	parser.compile(coroutine.wrap(function()
+		for key, value in pairs(node) do
+			coroutine.yield(value)
+		end
+	end), append)
+	return output
+end
+
+function parser.load_file(file)
+	local content = table.concat(parser.read_file(file))
+	if loadstring then
+		return loadstring(content)
+	else
+		return load(content)
+	end
+end
+
+function parser.do_file(file)
+	return parser.load_file(file)()
+end
+
+return parser
