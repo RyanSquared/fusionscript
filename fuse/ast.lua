a3675ba4e2dbe1d03444ac8a7e8a84841a61c4c9
Ryan Rion <charlesh2016@usd402.com>
2016-11-08 17:34:29 -0800

    ast.lua: add read_file abstraction and skip hashbangs

--- fuse/ast.lua
+++ fuse/ast.lua
@@ -2,17 +2,23 @@
 local lexer = require("fusion.core.lexer")
 local pretty = require("pl.pretty")
 
+local function read_file(file)
+	local file_handler = assert(io.open(file))
+	local line = file_handler:read()
+	if line:sub(1, 2) == "#!" then
+		pretty.dump(lexer:match(file_handler:read("*a")))
+	else
+		pretty.dump(lexer:match(line .. '\n' .. file_handler:read("*a")))
+	end
+	file_handler:close()
+end
 
 local args = {...}
 if #args > 1 then
 	for file in ipairs(args) do
 		print(("--- %s ---"):format(file))
-		local file_handler = assert(io.open(file))
-		pretty.dump(lexer:match(file_handler:read("*a")))
-		file_handler:close()
+		read_file(file)
 	end
 else
-	local file_handler = assert(io.open(args[1]))
-	pretty.dump(lexer:match(file_handler:read("*a")))
-	file_handler:close()
+	read_file(args[1])
 end
