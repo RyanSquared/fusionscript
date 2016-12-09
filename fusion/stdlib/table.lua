ffa61420c75821d65cd1031d416277b0bfd46ce8
ChickenNuggers <vandor2012@gmail.com>
2016-12-08 18:33:00 -0600

    table: table.from_generator and table.join

--- fusion/stdlib/table.lua
+++ fusion/stdlib/table.lua
@@ -3,6 +3,30 @@ function table.copy(t0)
 	for k, v in pairs(t0) do
 		t1[k] = v
 	end
+	return t1
+end
+
+function table.from_generator(iterable)
+	local t0 = {}
+	for k, v in iterable do
+		if v then
+			t0[k] = v
+		else
+			t0[#t0 + 1] = k
+		end
+	end
+	return t0
+end
+
+function table.join(t0, t1)
+	local t2 = {}
+	for k, v in pairs(t0) do
+		t2[k] = v
+	end
+	for k, v in pairs(t1) do
+		t2[k] = v
+	end
+	return t2
 end
 
 return table
