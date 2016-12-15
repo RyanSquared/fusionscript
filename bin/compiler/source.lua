cbbc25f9cf6145f4f0c16df5419b581f63a0e41c
ChickenNuggers <vandor2012@gmail.com>
2016-12-15 16:01:40 -0600

    fusionc-source: add -p and -h options

--- bin/compiler/source.lua
+++ bin/compiler/source.lua
@@ -2,22 +2,14 @@
 --- Generate a Lua file from FusionScript code.
 -- @script fusionc-source
 -- @author ChickenNuggers
--- @usage fusionc-source [FILE]/[DIRECTORY]
+-- @usage fusionc-source [OPTIONS] [FILE]
+-- fusionc-source [FILE]/[DIRECTORY]
+--
+--  -p | Write output to stdout
+--  -h | Print help information
 
 local parser = require("fusion.core.parsers.source")
 local lfs = require("lfs")
-
-local function process(file, does_output)
-	assert(file:match("%.fuse$"), ("Incorrect filetype: %s"):format(file))
-	local base = file:match("^(.+)%.fuse$")
-	local output = parser.read_file(file)
-	local output_file = io.open(base .. ".lua", "w")
-	output_file:write(output .. "\n")
-	output_file:close()
-	if does_output ~= false then
-		print(("Built file %s -> %s"):format(file, base .. ".lua"))
-	end
-end
 
 local function walk(file_func, dir)
 	for item in lfs.dir(dir) do
@@ -31,13 +23,43 @@ local function walk(file_func, dir)
 	end
 end
 
-local args = {...}
-if #args > 1 then
-	for file in ipairs(args) do
-		process(file)
+local function process(file, does_output)
+	if lfs.attributes(file, "mode") == "directory" then
+		return walk(process, file)
 	end
-elseif lfs.attributes(args[1], "mode") == "directory" then
-	walk(process, args[1])
-else
-	process(args[1])
+	local base = file:match("^(.+)%.fuse$")
+	local output = parser.read_file(file)
+	local output_file = io.open(base .. ".lua", "w")
+	output_file:write(output .. "\n")
+	output_file:close()
+	if does_output ~= false then
+		print(("Built file %s -> %s"):format(file, base .. ".lua"))
+	end
+end
+
+local args = {...}
+local arg_index = 1
+while arg_index <= #args do
+	local _arg = args[arg_index]
+	if _arg == "-p" then
+		arg_index = arg_index + 1
+		for i=arg_index, #args do
+			print(parser.read_file(args[i]))
+		end
+		break
+	elseif _arg == "--help" or _arg == "-h" then
+		local program = arg[0]:match(".+/(.-)$") or arg[0]
+		local usage = {
+			("Usage: %s [OPTIONS] [FILE]"):format(program);
+			("   or: %s [FILE/DIRECTORY]"):format(program);
+			("");
+			("\t-p | Write output to stdout");
+			("\t-h | Print help information")
+		}
+		print(table.concat(usage, "\n"))
+		break
+	else
+		process(_arg)
+		arg_index = arg_index + 1
+	end
 end
