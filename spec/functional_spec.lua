a54eef95535b746376fffb5f52fdc85847a21f68
ChickenNuggers <vandor2012@gmail.com>
2016-12-15 21:13:27 -0600

    functional_spec: add tests for iter() and more tests over any()/all()

--- spec/functional_spec.lua
+++ spec/functional_spec.lua
@@ -1,5 +1,6 @@
 describe("functional", function()
 	local fnl = require("fusion.stdlib.functional")
+	local itr = require("fusion.stdlib.iterable")
 	local table = require("fusion.stdlib.table")
 	it("can iterate correctly (_iter())", function()
 		local base_table = {
@@ -8,6 +9,23 @@ describe("functional", function()
 		}
 		for k, v in fnl._iter(base_table) do
 			assert.same(base_table[k], v)
+		end
+	end)
+	it("can iterate using another iterator", function()
+		local base_table = {
+			a = "hello";
+			b = "world";
+		}
+		for k, v in fnl._iter(pairs(base_table)) do
+			assert.same(base_table[k], v)
+		end
+	end)
+	it("can iterate over a table", function()
+		local base_table = {"a", "b", "c", "d"}
+		local num = 1
+		for char in fnl._iter("abcd") do
+			assert.same(base_table[num], char)
+			num = num + 1
 		end
 	end)
 	it("can create correct iterators (_mk_gen())", function()
@@ -57,6 +75,13 @@ describe("functional", function()
 		assert.same(false, fnl.any({false, false, false, false, false}))
 		assert.same(true, fnl.all({true, 1, 0, '', {}}))
 		assert.same(false, fnl.all({nil, false}))
+		local function is_even(n)
+			return n % 2 == 0
+		end
+		assert.same(true, fnl.any(fnl.map(is_even, itr.range(1, 10))))
+		assert.same(false, fnl.any(fnl.map(is_even, itr.range(1, 10, 2))))
+		assert.same(true, fnl.all(fnl.map(is_even, itr.range(2, 10, 2))))
+		assert.same(false, fnl.all(fnl.map(is_even, itr.range(1, 10, 2))))
 	end)
 	it("can reduce input streams by adding values", function()
 		assert.same(6, fnl.sum({1, 2, 3}))
