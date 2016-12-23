cb6d17caa223af42accdcc2bdfa28253c84c2b5b
ChickenNuggers <vandor2012@gmail.com>
2016-12-22 22:10:44 -0600

    ternary: add documentation

--- fusion/stdlib/ternary.lua
+++ fusion/stdlib/ternary.lua
@@ -1,3 +1,11 @@
+--- A single function to act as a ternary, right to left
+-- @module fusion.stdlib.ternary
+
+--- Return one of two values based on initial condition
+-- @function ternary
+-- @tparam boolean condition
+-- @param is_if Value to return if condition is truthy
+-- @param is_else Value to return if condition is not truthy
 return function(condition, is_if, is_else)
 	if condition then
 		return is_if
