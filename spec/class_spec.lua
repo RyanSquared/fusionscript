f5601cd2b26e6debf07e344056d7e9a8afcb837c
ChickenNuggers <vandor2012@gmail.com>
2016-12-23 16:52:15 -0600

    class_spec: initial commit

---
+++ spec/class_spec.lua
@@ -0,0 +1,28 @@
+local class = require("fusion.stdlib.class")
+
+local unpack = unpack or table.unpack -- luacheck: ignore 113
+describe("class", function()
+	it("can make a simple class", function()
+		local x = class({}, {}, "Test")
+		assert.same("Test", tostring(x))
+		local instance = x()
+		assert.same(x, instance.__class)
+		assert.is_nil(instance.__super)
+	end)
+	it("can make a complex class", function()
+		local ext = {3, 4}
+		local impl = {d = true}
+		local args = {'a', 1, false, nil, true}
+		local x = class({
+			d = 'b',
+			__init = function(self)
+				assert.is_not.same("function", tostring(self):sub(1, 8))
+			end
+		}, {extends = ext, implements = impl}, "Class")
+		assert.same(tostring(x), "Class")
+		local instance = x(unpack(args))
+		assert.same(x, instance.__class)
+		assert.same(ext, instance.__super)
+		assert.same(args, instance.__args)
+	end)
+end)
