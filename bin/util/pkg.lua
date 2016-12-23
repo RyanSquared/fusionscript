4dd41028a73d255eb618983c7e98e57666e9286c
ChickenNuggers <vandor2012@gmail.com>
2016-12-23 15:37:13 -0600

    fusion-pkg: initial commit (get and upgrade)

---
+++ bin/util/pkg.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+--- Manage packages for FusionScript
+-- @script fusion-pkg
+-- @author ChickenNuggers
+-- @usage fusion-pkg get ([USER]/[REPO]|git+[https|ssh]://<URL>)
+-- or: fusion-pkg upgrade
+
+local function clone(url)
+	os.execute((
+		"mkdir -p vendor; cd vendor; git clone %s --recursive; cd .."):format(
+		url))
+end
+
+local args = {...}
+if args[1] == "get" then
+	local url = args[2]
+	if url:match("^git+") then
+			clone(url:sub(5))
+	elseif url:find("/") then
+		clone("https://github.com/" .. url)
+	end
+elseif args[1] == "upgrade" then
+	local lfs = require("lfs")
+	lfs.chdir("vendor")
+	for dir in lfs.dir(".") do
+		if dir:sub(1, 1) ~= "." then
+			print("Upgrading " .. dir)
+			os.execute(("cd %s; git pull --recurse-submodules; cd .."):format(dir))
+		end
+	end
+	lfs.chdir("..")
+elseif not args[1] then
+	error("No command given")
+else
+	error("Unknown option: " .. args[1])
+end
