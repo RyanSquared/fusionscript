e10dff0ce94615eae89371eb98ab4c0102e4bb57
RyanSquared <vandor2012@gmail.com>
2017-05-08 19:48:45 -0500

    fusion-ast: Use new library naming system

--- bin/util/ast.lua
+++ bin/util/ast.lua
@@ -3,16 +3,16 @@
 -- @script fusion-ast
 -- @author ChickenNuggers
 -- @usage fusion-ast [FILE]
-local lexer = require("fusion.core.lexer")
+local parser = require("fusion.core.parser")
 local pretty = require("pl.pretty")
 
 local function read_file(file)
 	local file_handler = assert(io.open(file))
 	local line = file_handler:read()
 	if line:sub(1, 2) == "#!" then
-		pretty.dump(lexer:match(file_handler:read("*a")))
+		pretty.dump(parser:match(file_handler:read("*a")))
 	else
-		pretty.dump(lexer:match(line .. '\n' .. file_handler:read("*a")))
+		pretty.dump(parser:match(line .. '\n' .. file_handler:read("*a")))
 	end
 	file_handler:close()
 end
