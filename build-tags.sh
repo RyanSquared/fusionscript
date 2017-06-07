4718c9019a6b7b85eda33c0f0e16f0c5f87549da
RyanSquared <vandor2012@gmail.com>
2017-06-07 17:13:42 -0500

    Remove unnecessary, old files
    Files changed:
    - Makefile (removed)
    - build-tags.sh (removed)
    - update-lua-vm.sh (removed)

--- build-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# https://www.topbug.net/blog/2012/03/17/generate-ctags-files-for-c-slash-c-plus-plus-source-files-and-all-of-their-included-header-files/
-# ./ctags_with_dep.sh file1.c file2.c ... to generate a tags file for these files.
-
-g++ -M "$@" -std=c++11 | sed -e 's/[\\ ]/\n/g' | \
-        sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | \
-        ctags -L - --c++-kinds=+p --fields=+iaS --extra=+q
--- update-lua-vm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git fetch luavm && \
-git merge -s subtree --squash luavm/master && \
-git commit -m \
-"update luavm subtree to {$(git log luavm/master --oneline -1 --color=never)}"
