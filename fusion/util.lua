74bd8d4c660f43a65b8569bb1d5fb64c06f35fe2
RyanSquared <vandor2012@gmail.com>
2017-06-30 13:11:12 -0500

    fusion/util: Add ldoc tags

--- fusion/util.lua
+++ fusion/util.lua
@@ -1,3 +1,9 @@
+--- Utility libraries for FusionScript internal methods
+-- @module fusion.util
+
+--- Wrap `unpack()` and `table.unpack()`
+-- @function unpack
+
 return {
 	unpack = unpack or table.unpack -- luacheck: ignore
 }
