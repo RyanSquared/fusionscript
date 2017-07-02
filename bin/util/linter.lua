eefd1ec458ab6bf0ad1541c7f3a1a412df5ff7f9
RyanSquared <vandor2012@gmail.com>
2017-07-01 19:09:05 -0500

    fusion-lint: Add support for linting directories

--- bin/util/linter.lua
+++ bin/util/linter.lua
@@ -5,6 +5,7 @@
 local argparse = require("argparse")
 local parser = require("fusion.core.parser")
 local unpack = require("fusion.util").unpack
+local lfs = require("lfs")
 
 local argparser = argparse() {
 	name = "fusion-lint";
@@ -14,21 +15,42 @@ local argparser = argparse() {
 argparser:argument("file", "File(s) to lint"):args("*")
 argparser:flag("-i", "Read from input")
 argparser:option("--filename", "Filename to use when printing errors")
+argparser:option("--compiler",
+	"Compiler to use for checking compile-time errors", "source")
 local options = argparser:parse()
-local files = options.files
+local files = options.file
 
 local function lint_file(file_name, file)
 	local messages = {}
-	local is_linted, result = pcall(parser.match, nil, file:read("*a"))
-	if not is_linted then
+	local is_parsed, result = pcall(parser.match, parser, file:read("*a"),
+		file_name)
+	if not is_parsed then
 		local message = result.msg[2]
 		local pos = result.pos
 		messages[#messages + 1] =
 			("%s:%d:%d: (E001) %s"):format(file_name, pos.y, pos.x, message)
-	else -- luacheck: ignore
-		-- pass
+	else -- luacheck: ignore 542
+		-- ::TODO:: do the actual linting here
 	end
 	return messages
+end
+
+local function get_files(path, file_list)
+	-- recursively add all files found in a path to `file_list`
+	local file_type = assert(lfs.attributes(path, "mode"))
+	if file_type == "directory" then
+		for item in lfs.dir(path) do
+			if item:sub(1, 1) ~= "." then
+				get_files(path .. "/" .. item, file_list)
+			end
+		end
+	elseif file_type == "file" then
+		table.insert(file_list, {path, assert(io.open(path))})
+	else
+		error(("incorrect filetype for %s: %s (not file/directory)"):format(
+			path, file_type))
+	end
+	return file_list
 end
 
 if options.i then
@@ -36,11 +58,13 @@ if options.i then
 		print(line)
 	end
 else
-	for index, file in ipairs(files) do
-		files[index] = {file, assert(io.open(file))} -- TODO: support for dirs
+	local to_process = {}
+	for _, path in ipairs(files) do
+		for _, file in pairs(get_files(path, {})) do
+			table.insert(to_process, file)
+		end
 	end
-
-	for _, file in ipairs(files) do
+	for _, file in ipairs(to_process) do
 		for _, line in ipairs(lint_file(unpack(file))) do
 			print(line)
 		end
